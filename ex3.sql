f9ba868140943018ba60e29919136dc74ddc71db
arnargisla <arnargisla@gmail.com>
2017-10-24 01:07:46 +0200

    Fix bug, add requirements

--- ex3.sql
+++ ex3.sql
@@ -7,5 +7,7 @@ ON Orders.OrderID=OrderDetails.OrderID
 JOIN Products
 ON Products.ProductID=OrderDetails.ProductID
 GROUP BY Employees.EmployeeID
+HAVING TotalRevenue>10000000
+ORDER BY TotalRevenue DESC
 ;
 
