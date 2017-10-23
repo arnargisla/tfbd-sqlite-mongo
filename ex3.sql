8d394f8f9638212f9d9794d07dd50f4f5fd2818d
arnargisla <arnargisla@gmail.com>
2017-10-24 01:00:58 +0200

    Add ex3

---
+++ ex3.sql
@@ -0,0 +1,11 @@
+SELECT Employees.FirstName, Employees.LastName, Employees.Title, SUM(OrderDetails.Quantity) * SUM(Products.UnitPrice) AS TotalRevenue
+FROM Employees 
+LEFT JOIN Orders 
+ON Employees.EmployeeID=Orders.EmployeeID
+JOIN "Order Details" AS OrderDetails
+ON Orders.OrderID=OrderDetails.OrderID
+JOIN Products
+ON Products.ProductID=OrderDetails.ProductID
+GROUP BY Employees.EmployeeID
+;
+
