dd48e03694fcd42a912aacc2d020fecbb986b4ec
arnargisla <arnargisla@gmail.com>
2017-10-24 00:36:41 +0200

    Add comments and fix bug

--- arnar-ex2-mongo.js
+++ arnar-ex2-mongo.js
@@ -1,19 +1,32 @@
 // usage: mongo [--quiet] Northwind filename.js
 // Note: This script depends on the result from example 1
 
-var resultCollection = db.example2result
+// Create a collection for the result of ex2 
+var resultCollection = db.ex2ResultCollection
+// Make sure it is empty
 resultCollection.drop()
+
+// Find all the orders from ex1 that have more than 1 type
+// of product
 var orderIds = db.ex1ResultCollection.aggregate([
+  // The records are grouped by the OrderID attribute. In each group
+  // a 'total' attribute is added which holds information about 
+  // how many records had the same OrderID as the group record
   { "$group": { "_id": "$OrderID", "total": { "$sum": 1 } } },
+  // With $match the results are filtered so that only records
+  // that have 'total' greater than 1 are allowed.
   { "$match": { "total": { "$gt": 1 }}}
 ]).toArray().map(function(record){
+  // Only return the "_id" attribute, which corresponds to OrderID
   return record["_id"]
 })
 
-db.orderIdProductIdProductName.find({ "OrderID": { "$in": orderIds } }).forEach(function(record){
+// Add all the orders from ex1 that have ids in orderIds
+db.ex1ResultCollection.find({ "OrderID": { "$in": orderIds } }).forEach(function(record){
   resultCollection.insert(record)
 })
 
-db.resultCollection.find().forEach(function(record){
+// Print the result
+resultCollection.find().forEach(function(record){
   print(record.OrderID, record.ProductID, record.ProductName)
 });
